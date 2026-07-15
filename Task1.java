213733f19efed443a483b8c04ea5a390c4be1baa
haze <112647323+haze103@users.noreply.github.com>
2026-07-15 04:36:55 +0000

    completed Task1

---
+++ Task1.java
@@ -0,0 +1,10 @@
+/** 
+ * The Task1.java implements an application that
+ * simply prints "Kamusta Mundo!" to standard output.
+ */
+
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.println("Kamusta Mundo!"); //Display the string.
+    }
+}
