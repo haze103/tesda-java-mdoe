dc0aff8675327cb7ec0f57eb60510ecc93e0e7a5
haze <112647323+haze103@users.noreply.github.com>
2026-07-31 03:23:51 +0000

    Checked the requirements of the task and modified the if block that prints the output statement

--- Task5.java
+++ Task5.java
@@ -20,6 +20,6 @@ public class Task5 {
         if (intInput[0] == intInput[1] && intInput[1] == intInput[2])
             System.out.println("All numbers are equal.");
         else 
-            System.out.println(intLargestNum);
+            System.out.println("Largest Number = " + intLargestNum + ".");
     }
 }
